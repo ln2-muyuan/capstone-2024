1c773871a58ac5e608fc032672f8771896e05cb7
Muyuan <lmy2019@outlook.com>
2024-01-02 20:10:58 +0800

    first commit

---
+++ data-processing-py/ni.py
@@ -0,0 +1,23 @@
+import os
+import nibabel as nib
+import base64
+import numpy as np
+from PIL import Image
+
+
+nii_file = '20210119001723/T1GC.nii'
+nii_img = nib.load(nii_file)
+data = nii_img.get_fdata()
+
+
+output_dir = 'output/20210119001723/T1GC_png_slice'
+os.makedirs(output_dir, exist_ok=True)
+
+
+for i in range(data.shape[2]):
+    slice_data = data[:, :, i]
+    slice_data = np.uint8(slice_data)
+    image = Image.fromarray(slice_data)
+    output_file = os.path.join(output_dir, f'slice_{i}.png')
+    image.save(output_file)
+    print(f'Saved slice {i} as {output_file}')
